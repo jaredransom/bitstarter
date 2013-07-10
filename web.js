00d7910772f35f69bfa0294ad91dcf013efa7ec2
Jared Ransom <jared.r.ransom@gmail.com>
2013-07-10 01:41:45 +0000

    Pulled fs.readFileSync and buf.toString out

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
+var buf = fs.readFile(index.html);
+
+var index = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
